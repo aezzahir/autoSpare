57285528359004ec6296da2148b4d768f7c4307c
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-24 20:29:29 +0100

    classes

--- models/spear.py
+++ models/spear.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Spear(BaseModel, Base):
-    __tablename__ = 'spears'
-    Noun = Column(String(128), nullable=False)
-    NSN = Column(String(128), nullable=False)
-    Description = Column(String(128), nullable=False)
-    suppliers = relationship("Supplier", backref="spears")
+    __tablename__ = 'products'
+    designation = Column(String(128), nullable=False)
+    NSN = Column(Integer, nullable=False)
+    description = Column(String(128), nullable=False)
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'))
+    supplier = relationship("Supplier", back_populates="spears")
--- models/supplier.py
+++ models/supplier.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Supplier(BaseModel, Base):
     __tablename__ = 'suppliers'
-    Noun = Column(String(128), nullable=False)
-    Cage_code = Column(String(128), nullable=False)
-    Adress = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
+    cage_code = Column(Integer, nullable=False)
+    address = Column(String(128), nullable=False)
+    spears = relationship("Spear", back_populates="supplier")
